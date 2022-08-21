f034890e6c4dcc8dcde1d5eb1ef0855e4169f8b5
Chris Meller <chris@doesnthaveone.com>
2022-08-20 20:30:20 -0400

    Try fetching data manually.

---
+++ fetch.ts
@@ -0,0 +1,13 @@
+
+(async () => {
+    const response = await fetch('https://transpordiamet.ee/ohusoidukite-register', {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+        },
+    });
+
+    const body = await response.arrayBuffer();
+    const enc = new TextDecoder('utf-8');
+    const data = enc.decode(body);
+    await Deno.writeTextFile('data.html', data);
+})();

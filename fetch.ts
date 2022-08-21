4c2fefb0cbeff964d53fc2c93495769d52885898
Chris Meller <chris@doesnthaveone.com>
2022-08-21 13:00:17 -0400

    Remove function wrappers.
    Deno supports top-level await.

--- fetch.ts
+++ fetch.ts
@@ -1,17 +1,14 @@
 
-(async () => {
-    const response = await fetch('https://transpordiamet.ee/ohusoidukite-register', {
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-        },
-    });
 
-    const body = await response.arrayBuffer();
-    const enc = new TextDecoder('utf-8');
-    const data = enc.decode(body);
+const response = await fetch('https://transpordiamet.ee/ohusoidukite-register', {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+    },
+});
 
-    console.log('body', data);
+const body = await response.arrayBuffer();
+const enc = new TextDecoder('utf-8');
+const data = enc.decode(body);
 
-    console.log('Writing data.html...');
-    await Deno.writeTextFile('./data.html', data);
-})();
+console.log('Writing data.html...');
+await Deno.writeTextFile('./data.html', data);
--- parse.ts
+++ parse.ts
@@ -17,64 +17,62 @@ interface IAircraft {
     lastSeen: Date;
 }
 
-(async () => {
-    const html = await Deno.readTextFile('./data.html');
-    const doc = new DOMParser().parseFromString(html);
+const html = await Deno.readTextFile('./data.html');
+const doc = new DOMParser().parseFromString(html);
 
-    const table = xpath.select('.//table[ contains( ., "updated" ) ]', doc)[0] as Node;
+const table = xpath.select('.//table[ contains( ., "updated" ) ]', doc)[0] as Node;
 
-    // get the last updated date
-    const updatedDateCell = xpath.select('.//td[ contains( ., "updated" ) ]', table)[0] as Node;
-    const updatedDateContent = updatedDateCell.toString();
+// get the last updated date
+const updatedDateCell = xpath.select('.//td[ contains( ., "updated" ) ]', table)[0] as Node;
+const updatedDateContent = updatedDateCell.toString();
 
-    // some crazy parsing to get just the date
-    const updatedDate = updatedDateContent.split(':').map((c) => c.trim()).map((c) => c.split(/\s/));
-    console.log('updated: ', updatedDate[1][0]);
+// some crazy parsing to get just the date
+const updatedDate = updatedDateContent.split(':').map((c) => c.trim()).map((c) => c.split(/\s/));
+console.log('updated: ', updatedDate[1][0]);
 
-    // the total doesn't appear to be accurate
-    // get the total number of aircraft
-    // const totalCell = xpath.select('.//td[ contains( ., "total" ) ]', (table as Node));
-    // const totalContent = (totalCell[0] as Node).toString();
+// the total doesn't appear to be accurate
+// get the total number of aircraft
+// const totalCell = xpath.select('.//td[ contains( ., "total" ) ]', (table as Node));
+// const totalContent = (totalCell[0] as Node).toString();
 
-    // console.log('total', totalContent);
+// console.log('total', totalContent);
 
-    // get all but the first two rows, which contain our actual data
-    const rows = xpath.select('.//tr[ position() > 2 ]', table) as Node[];
+// get all but the first two rows, which contain our actual data
+const rows = xpath.select('.//tr[ position() > 2 ]', table) as Node[];
 
-    console.log('number of rows', rows.length);
+console.log('number of rows', rows.length);
 
-    const aircraft: IAircraft[] = []
-    for (const row of rows) {
-        // there are blank lines. if the registration is blank, skip this ling
-        if (xpath.select1('.//td[1]', row) == null) {
-            continue;
-        }
-
-        const registration = (xpath.select('.//td[2]', row)[0] as Node).textContent!.trim();
-        const type = (xpath.select('.//td[5]', row)[0] as Node).textContent!.trim();
-        const serialNumber = (xpath.select('.//td[6]', row)[0] as Node).textContent!.trim();
-        const owner = (xpath.select('.//td[7]', row)[0] as Node).textContent!.trim();
-        const operator = (xpath.select('.//td[8]', row)[0] as Node).textContent!.trim();
-
-        aircraft.push({
-            registration,
-            type,
-            serialNumber,
-            owner,
-            operator,
-            lastSeen: new Date(),
-        });
+const aircraft: IAircraft[] = []
+for (const row of rows) {
+    // there are blank lines. if the registration is blank, skip this ling
+    if (xpath.select1('.//td[1]', row) == null) {
+        continue;
     }
 
-    console.log('number of aircraft', aircraft.length);
+    const registration = (xpath.select('.//td[2]', row)[0] as Node).textContent!.trim();
+    const type = (xpath.select('.//td[5]', row)[0] as Node).textContent!.trim();
+    const serialNumber = (xpath.select('.//td[6]', row)[0] as Node).textContent!.trim();
+    const owner = (xpath.select('.//td[7]', row)[0] as Node).textContent!.trim();
+    const operator = (xpath.select('.//td[8]', row)[0] as Node).textContent!.trim();
 
-    const updatedAt = parse(updatedDate[1][0], 'dd.MM.yyyy', new Date());
+    aircraft.push({
+        registration,
+        type,
+        serialNumber,
+        owner,
+        operator,
+        lastSeen: new Date(),
+    });
+}
 
-    const result: IResult = {
-        lastUpdated: updatedAt,
-        total: aircraft.length,
-        aircraft: aircraft,
-    };
+console.log('number of aircraft', aircraft.length);
 
-    await Deno.writeTextFile('./data.json', JSON.stringify(result, null, 2));
-})();
+const updatedAt = parse(updatedDate[1][0], 'dd.MM.yyyy', new Date());
+
+const result: IResult = {
+    lastUpdated: updatedAt,
+    total: aircraft.length,
+    aircraft: aircraft,
+};
+
+await Deno.writeTextFile('./data.json', JSON.stringify(result, null, 2));
